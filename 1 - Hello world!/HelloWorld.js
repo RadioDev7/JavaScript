af2931a6b619ee6ae33eb53d704810bb010a1b62
RadioDev7 <125562290+RadioDev7@users.noreply.github.com>
2023-07-17 10:23:19 +0300

    Add files via upload

---
+++ 1 - Hello world!/HelloWorld.js
@@ -0,0 +1,4 @@
+alert("Hello!");
+console.log('Hello, World!');
+console.log('Hello, World!');
+// document.oncontextmenu = function(e){return false}; блокировка мыши
---
+++ 2 - изучение основ/js/script.js
@@ -0,0 +1,39 @@
+console.log("Hello Js!") // Ввод в консоль
+console.log(8 + 75) // Ввод в консоль с решением примера
+
+//       Переменные "Данные"
+// var let const - существуют в JS
+let age = 19
+console.log("Возраст -", age)
+// const age = 19
+// console.log("Возраст -", const)
+// не работает
+
+// Название данных должно соответствовать реальному объекту.
+// Имя = Name, Айди картв = idCard 
+
+
+//        Типы данных
+// String - строка, Number - число, Boolean - true или false, 
+// Undefined - значение не определено, Null - Отсутствие какого-либо объектного значения
+// Object - Cсылочный тип данных, является то самостоятельной единицей, имеющей свойства и определённый тип.
+// Есть и другие но они используются в специфических задачах
+
+//String
+const name = "Vasily"
+
+//Number
+const ageConst1 = 20; // Ковычки обязательны (;)
+
+// Boolean
+const hungry = true;
+
+// Undefined
+let dog = "Palkan";
+let dog1;
+// Вывод примера
+console.log(dog) 
+console.log(dog1) 
+
+// null 
+let dog2 = null;
